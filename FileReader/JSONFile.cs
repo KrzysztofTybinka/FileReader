00500fca6be64b58ccd3ffc1fe34c8b9e5daa504
Krzysztof Tybinka <ktybinka@interia.pl>
2023-02-03 14:13:13 +0100

    Created JSONFile class

---
+++ FileReader/JSONFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileReader
+{
+    /// <summary>
+    /// Initilizes a new instance of the JSONFile class.
+    /// Represents a JSON file.
+    /// </summary>
+    public class JSONFile
+    {
+    }
+}
