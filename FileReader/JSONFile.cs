acef3a8c9e40b51d1f1fae02a9e096d3d9290dd8
Krzysztof Tybinka <ktybinka@interia.pl>
2023-02-04 00:57:27 +0100

    IsValidPath method added

--- FileReader/JSONFile.cs
+++ FileReader/JSONFile.cs
@@ -21,6 +21,18 @@ namespace FileReader
 
         }
 
+        private bool IsValidUrl(string url)
+        {
+            FileInfo fi = new FileInfo(url);
+            return fi.Extension == ".json";
+        }
+
+        private bool IsValidPath(string path)
+        {
+            FileInfo fi = new FileInfo(path);
+            return fi.Extension == ".json";
+        }
+
 
     }
 }
