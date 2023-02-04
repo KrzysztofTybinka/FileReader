47cc07e7c582214ca9de0badafa8e8f80bf48ada
Krzysztof Tybinka <ktybinka@interia.pl>
2023-02-04 01:00:38 +0100

    CreateFromUrl method added

--- FileReader/JSONFile.cs
+++ FileReader/JSONFile.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,6 +20,26 @@ namespace FileReader
         public JSONFile()
         {
 
+        }
+
+        public bool CreateFromUrl(string url)
+        {
+            try
+            {
+                if (IsValidUrl(url))
+                {
+                    using (var webClient = new WebClient())
+                    {
+                        file = (JObject)webClient.DownloadString(url);
+                        return true;
+                    }
+                }
+                throw new Exception();
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
 
         private bool IsValidUrl(string url)
