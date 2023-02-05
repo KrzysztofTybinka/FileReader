df0e31f288219ac8236a5c063b9c53ed87682851
Krzysztof Tybinka <ktybinka@interia.pl>
2023-02-05 15:05:03 +0100

    IsValidUrl method added

--- FileReader/XMLFile.cs
+++ FileReader/XMLFile.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace FileReader
 {
-    internal class XMLFile
+    public class XMLFile
     {
+        private XDocument file = new XDocument();
+        public XMLFile(string url)
+        {
+            file = DownloadFileAsync(url).Result;
+        }
+
+        private async Task<XDocument> DownloadFileAsync(string url)
+        {
+            HttpClient client = new HttpClient();
+            string xmlString = await client.GetStringAsync(url);
+            return XDocument.Parse(xmlString);
+        }
     }
 }
