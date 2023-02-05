6c6f9d6baa9a8dfb47fe75ca600f11794feb8a28
Krzysztof Tybinka <ktybinka@interia.pl>
2023-02-05 23:25:17 +0100

    summary for CSVFile methods added

--- FileReader/CSVFile.cs
+++ FileReader/CSVFile.cs
@@ -8,6 +8,10 @@ using System.Xml.Linq;
 
 namespace FileReader
 {
+    /// <summary>
+    /// Initilizes a new instance of the CSVFile class.
+    /// Represents a CSV file.
+    /// </summary>
     public class CSVFile : IFile
     {
         private string file;
@@ -17,26 +21,12 @@ namespace FileReader
             file = DownloadFileAsync(url).Result;
         }
 
-
-        private async Task<string> DownloadFileAsync(string url)
-        {
-            try
-            {
-                if (IsValidUrl(url))
-                {
-                    HttpClient client = new HttpClient();
-                    string csvString = await client.GetStringAsync(url);
-                    return csvString;
-                }
-                throw new Exception();
-            }
-            catch (Exception)
-            {
-                throw new FileLoadException("File not loaded.");
-            }
-        }
-
-
+        /// <summary>
+        /// Deserializes CSV string representation
+        /// and parses it to list of objects.
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns>List of objects.</returns>
         public List<object> ObjectsToList(string root)
         {
             try
@@ -57,7 +47,35 @@ namespace FileReader
             }
         }
 
+        /// <summary>
+        /// Downloads CSV content as string from given url.
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns>String with downloaded csv content.</returns>
+        /// <exception cref="FileLoadException"></exception>
+        private async Task<string> DownloadFileAsync(string url)
+        {
+            try
+            {
+                if (IsValidUrl(url))
+                {
+                    HttpClient client = new HttpClient();
+                    string csvString = await client.GetStringAsync(url);
+                    return csvString;
+                }
+                throw new Exception();
+            }
+            catch (Exception)
+            {
+                throw new FileLoadException("File not loaded.");
+            }
+        }
 
+        /// <summary>
+        /// Checks if file type from given url is of type CSV.
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns>True if file is CSV type, otherwise false.</returns>
         private bool IsValidUrl(string url)
         {
             try
