4439400837c457da3d37c80a218bbd4c7ab074d5
Krzysztof Tybinka <ktybinka@interia.pl>
2023-02-06 01:07:02 +0100

    Start method added

--- FileReader/Dialogues.cs
+++ FileReader/Dialogues.cs
@@ -11,6 +11,17 @@ namespace FileReader
     /// </summary>
     public static class Dialogues
     {
-
+        /// <summary>
+        /// Starting method writing o console basic
+        /// application information.
+        /// </summary>
+        public static void Start()
+        {
+            Console.WriteLine("File Reader is a command-line interface application that provides \n " +
+                "methods to create files in different formats, download files from given url, " +
+                "unzip, deserialize and/or read them. \n");
+            Console.WriteLine();
+            Console.WriteLine("Accessible file types: XML, JSON, CSV. (aslo in ZIP format)");
+        }
     }
 }
