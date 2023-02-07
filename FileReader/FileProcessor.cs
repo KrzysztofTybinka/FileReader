fede921f31890316b0aa70885110ea2526799c2c
Krzysztof Tybinka <ktybinka@interia.pl>
2023-02-07 17:35:13 +0100

    FileProcessor class summarized

--- FileReader/FileProcessor.cs
+++ FileReader/FileProcessor.cs
@@ -8,8 +8,19 @@ using File = FileReader.Files.File;
 
 namespace FileReader
 {
+    /// <summary>
+    /// Provides methods for file serialization and creation.
+    /// </summary>
     public class FileProcessor
     {
+        /// <summary>
+        /// Determies what type of file to create and creates it.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="type"></param>
+        /// <param name="content"></param>
+        /// <returns>File object.</returns>
+        /// <exception cref="InvalidOperationException"></exception>
         public File CreateFile(string name, string type, Dictionary<string, List<string>> content)
         {
             File? file = null;
@@ -35,7 +46,15 @@ namespace FileReader
             return file;
         }
 
-
+        /// <summary>
+        /// Creates a new instance of a file object, either a JSONFile,
+        /// XMLFile, or CSVFile, depending on the value of type.
+        /// </summary>
+        /// <param name="content"></param>
+        /// <param name="name"></param>
+        /// <param name="type"></param>
+        /// <returns>File object.</returns>
+        /// <exception cref="InvalidOperationException"></exception>
         public File DeserializeFile(string content, string name, string type)
         {
             File? file = null;
@@ -61,6 +80,12 @@ namespace FileReader
             return file;
         }
 
+        /// <summary>
+        /// Takes a "File" object as input and returns the
+        /// serialized string representation of the file.
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns>String representation of the file.</returns>
         public string SerializeFile(File file)
         {
             return file.Serialize();
